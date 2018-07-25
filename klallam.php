0d6c121de673ad8020ebde46fc3a8653a92a6ab0
Lou Wilson <gluefish@gluebook.local>
2018-07-25 14:25:29 -0700

    Fixed instance of wrong file in place

--- klallam.php
+++ klallam.php
@@ -1,14 +1,13 @@
 <?php
-//klallam2.php - the php portion of the project Klallam-Lookup.
 $addy="Location: https://docs.google.com/spreadsheets/d/";
 $key="1_fhOXr_qdWYnZG1_N5ueNAdsusbQPrhp1uTOXU7wF_g/gviz/";
 $tq="tq?tqx=out:html&tq=";
 $addy = $addy . $key . $tq;
 $addy = $addy . "SELECT A, B, C, D WHERE A CONTAINS '" . $_GET["en"] . "'";
 $addy = $addy . " OR B CONTAINS '" . $_GET["en"] . "'";
-$addy = $addy . " OR D CONTAINS '" . $_GET["en"] . "'";
+$addy = $addy . " OR D CONTAIND '" . $_GET["en"] . "'";
 $addy = $addy . " OR A CONTAINS 'ENGLISH'";
-//echo($addy);  //for debugging
+//echo($addy);
 header($addy);
 exit();
 ?>
